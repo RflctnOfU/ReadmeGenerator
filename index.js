8bc305b6e19ab1ad39566b1beeeb1a630046a459
Kris Marshall <rflctnofu@hotmail.com>
2022-07-14 23:38:40 -0400

    I think I got the markdown writing function set...figuring out how to call that

--- index.js
+++ index.js
@@ -2,16 +2,84 @@
 const inquirer = require('inquirer')
 const fs = require('fs')
 const path = require('path')
-const markdown = require('./utils/generateMarkdown')
+const markdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title'
+    },
+    {
+        type: 'input',
+        message: 'Enter a description of your project.',
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message: 'What, if any, are the instructions for installation?',
+        name: 'install',
+    },
+    {
+        type: 'input',
+        message: 'Any information about usage?',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'Please enter contribution guidelines',
+        name: 'contribution',
+    },
+    {
+        type: 'input',
+        message: 'Please enter any test instructions.',
+        name: 'test',
+    },
+    {
+        type: 'list',
+        message: 'Under which license will this app be covered?',
+        name: 'license',
+        choices: ['MIT', 'ISC', 'GNU GPLv3', 'Apache License 2.0']
+    },
+    {
+        type: 'input',
+        message: 'Please enter your GitHub user name.',
+        name: 'github'
+    },
+    {
+        type: 'input',
+        message: 'Please enter your email address?',
+        name: 'email'
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
+function writeToFile(fileName, data) {
+    // fs.writeFile('readme.json', JSON.stringify(fileName, null, '\t'), (err) => err ? console.error(err) : console.log('success'));
+    markdown(fileName);
+    fs.writeFile('README2.md', generateMarkdown(data), (err) => err ? console.error(err) : console.log('big success!'))
+}
 
 // TODO: Create a function to initialize app
-function init() { }
+function init() {
+    inquirer.prompt(questions)
+        .then((response) =>
+            fs.writeFile('readme.json', JSON.stringify(response, null, '\t'), (err) => err ? console.error(err) : console.log('success')))
+
+
+}
 
 // Function call to initialize app
 init();
+
+
+
+
+
+
+
+
+
+// Description:
+// 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,73 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) { }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) { }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) { }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ---
+  ## Description
+
+  ${data.description}
+
+  ---
+  ## Table of Contents
+
+  -[Installation](#installation)
+  -[Usage](#usage)
+  -[License](#license)
+  -[Contributing](#contributing)
+  -[Tests](#tests)
+  -[Questions](#questions)
+
+  ---
+  ## Installation {#installation}
+
+  \`\`\`bash
+  ${data.install}
+  \`\`\`
+
+  ---
+  ## Usage {#usage}
+
+  \`\`\`
+  ${data.usage}
+  \`\`\`
+
+  ---
+  ## License
+
+  \`\`\`md
+  ${renderLicenseSection()}
+  \`\`\`
+
+  ---
+  ## Contributing {#contributing}
+
+  If you would like to contribute, please adhere to the following guidelines:
+  >${data.contribution}
+
+  ---
+  ## Tests {#tests}
+
+  How to run my tests:
+  >${data.test}
+  
+  ---
+  ## Questions {#questions}
+
+  My [Github](github.com/${data.github}) profile.
+
+  If you have questions, send me an [email](${data.email})
 `;
 }
 
